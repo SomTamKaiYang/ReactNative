e71cad883c15ad069fada28ffd8753d626dacdbb
SomTamKaiYang <ko.natthaphum_st@tni.ac.th>
2023-07-29 09:34:16 +0700

    Recap

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
-import ViewBoxesWithColorAndText from "./components/ViewBoxesWithColorAndText";
-// import DisplayandImage from "./components/DisplayandImage";
-// import LotsOfGreeting from "./components/LotsOfGreeting";
-// import Customtext from "./components/Customtext";
-// import Counter from "./components/Counter";
-// import Myinput from "./components/Myinput";
-// import IncrementCounter from "./components/IncrementCounter";
-// import Form from "./components/Form";
-import Login from "./components/Login";
+import Recap from "./components/Recap";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <ViewBoxesWithColorAndText/> */}
-      {/*<DisplayandImage />*/}
-      {/*<LotsOfGreeting/>*/}
-      {/* <Customtext/> */}
-      {/* <Counter/>  */}
-      {/* <Myinput/> */}
-      {/* <IncrementCounter /> */}
-      {/* <Form/> */}
-      <Login/>
+    <View style={{flex: 1,alignItems: "center"}}>
+      <Recap/>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/Recap.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import React, { useState } from "react";
+
+const Recap = () => {
+  const [Email, setEmail] = useState("");
+  const [Pass, setPass] = useState("");
+
+  const alertt = () => {
+    alert("Email: " + Email + "\nPassword: " + Pass);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Email "
+        style={styles.input}
+        value={Email}
+        onChangeText={(value) => {
+          setEmail(value);
+        }}
+      />
+      <TextInput
+        placeholder="Password "
+        style={styles.input}
+        value={Pass}
+        onChangeText={(value) => {
+          setPass(value);
+        }}
+      />
+      <Text>{"\n"}</Text>
+      <Button style={styles.submitButton} title="SUBMITT" onPress={alertt()} />
+    </View>
+  );
+};
+
+export default Recap;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 23,
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: "#7a42f4",
+    borderWidth: 0.5,
+  },
+  submitButton: {
+    backgroundColor: "#7a42f4",
+    padding: 10,
+    margin: 15,
+    height: 40,
+  },
+  submitButtonText: {
+    color: "white",
+  },
+});
