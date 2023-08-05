77e596bd84e9bb4cf681a3f2ea3ff444abb28d17
SomTamKaiYang <136871402+SomTamKaiYang@users.noreply.github.com>
2023-08-05 11:38:00 +0700

    useEffect

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import TouchablePractice from './components/TouchablePractice'
+// import TouchablePractice from './components/TouchablePractice'
 // import Example_useEffect from './components/Example_useEffect'
-//import UseEffectFlatList from './components/UseEffectFlatList'
+import UseEffectFlatList from './components/UseEffectFlatList'
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
-      {<TouchablePractice/>}
+    <View style={styles.container}>
+      {/* {<TouchablePractice/>} */}
       {/* <Example_useEffect/> */}
-      {/* {<UseEffectFlatList/>} */}
+      {<UseEffectFlatList/>}
     </View>
   )
 }
