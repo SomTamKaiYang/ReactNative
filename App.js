7a807e4f0fdbbf622d2c4f7c042a344ef03b5dc9
SomTamKaiYang <ko.natthaphum_st@tni.ac.th>
2023-08-19 10:12:11 +0700

    RandomUserScreen

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 // import TouchablePractice from './components/TouchablePractice'
 // import Example_useEffect from './components/Example_useEffect'
-import UseEffectFlatList from './components/UseEffectFlatList'
+// import UseEffectFlatList from './components/UseEffectFlatList'
+import RandomUserScreen from './components/RandomUserScreen'
 
 const App = () => {
   return (
     <View style={styles.container}>
+      {<RandomUserScreen/>}
       {/* {<TouchablePractice/>} */}
       {/* <Example_useEffect/> */}
-      {<UseEffectFlatList/>}
+      {/* {<UseEffectFlatList/>} */}
     </View>
   )
 }
