fd1deb5756d047e8f906ea3384b870930b4539ae
SomTamKaiYang <136871402+SomTamKaiYang@users.noreply.github.com>
2023-08-19 10:15:10 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import TouchablePractice from './components/TouchablePractice'
+// import TouchablePractice from './components/TouchablePractice'
 // import Example_useEffect from './components/Example_useEffect'
 // import UseEffectFlatList from './components/UseEffectFlatList'
+import RandomUserScreen from './components/RandomUserScreen'
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
-      {<TouchablePractice/>}
+    <View style={styles.container}>
+      {<RandomUserScreen/>}
+      {/* {<TouchablePractice/>} */}
       {/* <Example_useEffect/> */}
       {/* {<UseEffectFlatList/>} */}
     </View>
