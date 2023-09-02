67dffc9a5dec70b7508563adfe8a0be71eb5ec59
SomTamKaiYang <ko.natthaphum_st@tni.ac.th>
2023-09-02 10:21:12 +0700

    Exercise

--- App.js
+++ App.js
@@ -1,35 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-// import TouchablePractice from './components/TouchablePractice'
-// import Example_useEffect from './components/Example_useEffect'
-// import UseEffectFlatList from './components/UseEffectFlatList'
-// import RandomUserScreen from './components/RandomUserScreen'
-// import FlatList_headerFooter from './components/FlatList_headerFooter'
-// import News from './components/News'
-// import FlatListAPI from './components/FlatListAPI'
-import ProductScreen from './components/ProductScreen'
+import HomeScreen from './components/HomeScreen'
+import DetailScreen from './components/DetailScreen'
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
-      {<ProductScreen/>}
-      {/* {<News/>} */}
-      {/* {<FlatListAPI/>} */}
-      {/* {<FlatList_headerFooter/>} */}
-      {/* {<RandomUserScreen/>} */}
-      {/* {<TouchablePractice/>} */}
-      {/* <Example_useEffect/> */}
-      {/* {<UseEffectFlatList/>} */}
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={HomeScreen} options={{title: 'Overview'}}/>
+            <Stack.Screen name='Details' component={DetailScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-  }
-})
+const styles = StyleSheet.create({})
--- App1.js
+++ App1.js
@@ -1,22 +1,78 @@
-import { StyleSheet, Text, View } from "react-native";
-// import Recap from "./components/Recap";
-import DefineStyle from "./components/DefineStyle";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+// import TouchablePractice from './components/TouchablePractice'
+// import Example_useEffect from './components/Example_useEffect'
+// import UseEffectFlatList from './components/UseEffectFlatList'
+// import RandomUserScreen from './components/RandomUserScreen'
+// import FlatList_headerFooter from './components/FlatList_headerFooter'
+// import News from './components/News'
+// import FlatListAPI from './components/FlatListAPI'
+import ProductScreen from './components/ProductScreen'
 
-
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <DefineStyle/>
+    <View style={{flex:1}}>
+      {<ProductScreen/>}
+      {/* {<News/>} */}
+      {/* {<FlatListAPI/>} */}
+      {/* {<FlatList_headerFooter/>} */}
+      {/* {<RandomUserScreen/>} */}
+      {/* {<TouchablePractice/>} */}
+      {/* <Example_useEffect/> */}
+      {/* {<UseEffectFlatList/>} */}
     </View>
-  );
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  }
+})
+
+
+/* import {Text, View,Button } from 'react-native'
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+function HomeScreen({navigation}){
+    return(
+        <View style = {{flex:1,alignItems:'center',justifyContent:'center'}}>
+            <Text>HomeScreen</Text>
+            <Button
+             title='Go to Detail'
+             onPress={()=>navigation.navigate('Details')}
+            />
+        </View>
+    )
+}
+
+function DetailsScreen(){
+  return(
+    <View style = {{flex:1,alignItems:'center',justifyContent:'center'}}>
+            <Text>Detail Screen</Text>
+    </View>
+  )
 }
 
 
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={HomeScreen} options={{title: 'Overview'}}/>
+            <Stack.Screen name='Details' component={DetailsScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App */
